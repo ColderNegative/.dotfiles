f6be6f04ab4816106c2eaed69517324be0ac2517
lquinnMac <lewis.quinn.22@ucl.ac.uk>
2023-04-11 22:40:51 -0500

    updated to astronvim

---
+++ .config/nvim.bak/init.lua
@@ -0,0 +1,7 @@
+-- fundamental configuration
+require("settings")
+require("plugins")
+require("keybindings")
+
+-- colorscheme
+vim.cmd('colorscheme gruvbox-material')
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,20 @@
--- fundamental configuration
-require("settings")
-require("plugins")
-require("keybindings")
+if vim.loader then vim.loader.enable() end -- enable vim.loader early if available
 
--- colorscheme
-vim.cmd('colorscheme gruvbox-material')
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
+
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify("Error setting up colorscheme: " .. astronvim.default_colorscheme, "error")
+  end
+end
+
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
