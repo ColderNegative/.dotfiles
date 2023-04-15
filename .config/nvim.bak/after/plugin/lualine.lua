25d15d55f8944089b8e0abf09572b196d4956c66
lquinnMac <lewis.quinn.22@ucl.ac.uk>
2023-04-15 11:57:30 -0500

    deleted old files

--- .config/nvim.bak/after/plugin/lualine.lua
+++
@@ -1,8 +0,0 @@
-require('lualine').setup {
-    options = {
-        theme = 'gruvbox-material',
-        section_separators = '',
-        component_separators = '',
-        icons_enabled = false,
-    }
-}
--- .config/nvim.bak/init.lua
+++
@@ -1,7 +0,0 @@
--- fundamental configuration
-require("settings")
-require("plugins")
-require("keybindings")
-
--- colorscheme
-vim.cmd('colorscheme gruvbox-material')
